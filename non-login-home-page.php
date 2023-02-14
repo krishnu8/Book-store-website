6d8a8191511beac09246d10d7fcbdd683f21ed6a
aahan06 <mmiyan584@rku.ac.in>
2023-02-15 01:02:10 +0530

    aa

--- non-login-home-page.php
+++ non-login-home-page.php
@@ -13,7 +13,7 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 
-    <link rel="stylesheet" href="product.css">
+    <link rel="stylesheet" href="css/product.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <title>Home Page</title>
 </head>
